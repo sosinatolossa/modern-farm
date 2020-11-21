71bff05cf58b7b0486fc0578f6d6d80a4393821f
sosinatolossa <sosinatolossa16@gmail.com>
2020-11-21 15:57:11 -0600

    added harvester.js file

---
+++ src/scripts/harvester.js
@@ -0,0 +1,11 @@
+import {plantseeds} from "./tractor.js"
+
+export const harvestPlants = (thePlantsArray) => {
+    for (let index = 0; index < 3; index++) {
+        for (let j = 0; j < 6; j++) {
+            if (thePlantArray[index][j] === number) {
+                console.log("checking number",number)
+            }
+        }
+    }
+}
