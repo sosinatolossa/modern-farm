50e63ebd5ca9aeadf1ec8217724d1d9998c2c9a3
sosinatolossa <sosinatolossa16@gmail.com>
2020-11-21 16:32:41 -0600

    added some code in main.js

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,9 +1,10 @@
-import {plantseeds} from "./tractor.js"
+import {plantSeeds} from "./tractor.js"
 
 export const harvestPlants = (thePlantsArray) => {
     for (let index = 0; index < 3; index++) {
         for (let j = 0; j < 6; j++) {
-            if (thePlantArray[index][j] === number) {
+            if (thePlantsArray[index] === "output") {
+                thePlantsArray[index] * thePlantsArray[index].value
                 console.log("checking number",number)
             }
         }
